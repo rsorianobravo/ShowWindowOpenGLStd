a637efc84819fd07862a0950b4afadd92d7e8e1b
raulsb <raul.soriano@nunegal.com>
2023-07-03 15:39:29 +0200

    [ADD] Shader and Fragment

--- ShowWindowOpenGLStd/Main.cpp
+++ ShowWindowOpenGLStd/Main.cpp
@@ -1,20 +1,30 @@
 #include "libs.h"
 
 
+/*********************************************************************************************/
+
+// method to calculate the frameBuffer
 void framebuffer_resize_callback(GLFWwindow* wimdow, int fbw, int fbh)
 {
 	glViewport(0, 0, fbw, fbh);
-
 }
 
+/*********************************************************************************************/
+
+bool loadShaders(GLuint &program)
+{
+	char infoLog[512];
+	GLuint success;
+}
+/*********************************************************************************************/
 
 int main()
 {
 
-	// Init GLFW
+	// ------ Init GLFW lib
 	glfwInit();
 
-	// Create Window
+	// ------ Create Window
 	const int WINDOW_WIDTH = 640;
 	const int WINDOW_HEIGHT = 480;
 	int framebufferWidth = 0;
@@ -27,23 +37,24 @@ int main()
 
 	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
-	// macOS
+	// ------ macOS Compatibility
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 
 
 	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "1", NULL, NULL);
 
-	//glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
-	glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
-	glViewport(0, 0, framebufferWidth, framebufferHeight);
+	glfwSetFramebufferSizeCallback(window, framebuffer_resize_callback);
+	//glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
+	//glViewport(0, 0, framebufferWidth, framebufferHeight);
 
-	// Important
+	// ------ Important
 	glfwMakeContextCurrent(window);
 
-	// Init Glew
+	// ------ Init Glew
 	glewExperimental = GL_TRUE;
 
+	// ------ Check the init status of Glew 
 	if (glewInit() != GLEW_OK)
 	{
 		std::cout << "ERROR" << "\n";
@@ -54,30 +65,43 @@ int main()
 
 	}
 
-	// Main Loop
+	// ------ Shader Init
+	GLuint core_program;
+	loadShaders(core_program);
+
+
+	// ------ Model
+
+	// ------ VAO VBO
+
+
+
+
+
+	// ------ Main Loop
 	while (!glfwWindowShouldClose(window))
 	{
-		// Check update input
+		// ------ Check update input
 		//glfwSetWindowShouldClose(window, true);
 		glfwPollEvents();
 
-		// Update
+		// ------ Update
 
-		// Draw
+		// ------ Draw
 
-		// Clear
+		// ------ Clear
 		glClearColor(0.345f, 0.f, 0.f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-		// Draw
+		// ------ Draw
 
-		// End Draw
+		// ------ End Draw
 		glfwSwapBuffers(window);
 		glFlush();
 	}
 
 
-	// End Program
+	// ------ End Program
 	glfwTerminate();
 
  	return 0;
