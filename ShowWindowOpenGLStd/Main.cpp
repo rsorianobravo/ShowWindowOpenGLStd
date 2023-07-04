0587245eea088057c084cad751146ebff650a761
raulsb <raul.soriano@nunegal.com>
2023-07-04 09:09:18 +0200

    [ADD] OenGL Options enable and UpdateInput to close the window

--- ShowWindowOpenGLStd/Main.cpp
+++ ShowWindowOpenGLStd/Main.cpp
@@ -3,6 +3,17 @@
 
 /*********************************************************************************************/
 
+// ------ Talk about sending sizeof pointers and errors
+void updateInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
+
+/*********************************************************************************************/
+
 // ------ method to calculate the frameBuffer
 void framebuffer_resize_callback(GLFWwindow* wimdow, int fbw, int fbh)
 {
@@ -160,6 +171,17 @@ int main()
 
 	}
 
+	// ------ OpenGL Options
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CCW);
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
 	// ------ Shader Init
 	GLuint core_program;
 	if (!loadShaders(core_program))
@@ -184,7 +206,7 @@ int main()
 		glfwPollEvents();
 
 		// ------ Update
-
+		updateInput(window);
 		// ------ Draw
 
 		// ------ Clear
