821d08b4076ead5e599257c6da24f79d65fc158b
raulsb <raul.soriano@nunegal.com>
2023-07-06 08:51:17 +0200

    [ADD] Shacer header

--- ShowWindowOpenGLStd/Shader.h
+++ ShowWindowOpenGLStd/Shader.h
@@ -17,3 +17,38 @@
 #include<glm/gtc/matrix_transform.hpp>
 #include<glm/gtc/type_ptr.hpp>
 
+class Shader
+{
+private:
+
+	GLuint id;
+
+	std::string loadShaderSource(char* fileName)
+	{
+		return "";
+	}
+
+	GLuint loadShader(GLenum type, char* fileName)
+	{
+
+	}
+
+	void linkProgram(GLuint vertexShader, GLuint geometryShader, GLuint fragmentShader)
+	{
+
+	} 
+
+public:
+
+	Shader(char* vertexFile, char* geometryFile, char* fragmentFile)
+	{
+
+	}
+
+	~Shader()
+	{
+		glDeleteProgram(this->id);
+	}
+
+	// Set uniform functions
+};
