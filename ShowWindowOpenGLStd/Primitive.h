f09d948834f284c6c8205b4e4b5fd50fb965e178
raulsb <raul.soriano@nunegal.com>
2023-07-19 11:50:07 +0200

    [ADD] Triangle data

--- ShowWindowOpenGLStd/Primitive.h
+++ ShowWindowOpenGLStd/Primitive.h
@@ -69,14 +69,14 @@ public:
 		{
 			// Position , Color, Texcoords
 
-			glm::vec3(-0.5f,0.14f,0.f), glm::vec3(1.f,0.f,0.f), glm::vec2(0.f, 1.f), glm::vec3(0.f,0.f,1.f),
-			glm::vec3(-0.5f,-0.14f,0.f), glm::vec3(0.f,1.f,0.f), glm::vec2(0.f,0.f), glm::vec3(0.f,0.f,1.f),
-			glm::vec3(0.5f,-0.14f,0.f), glm::vec3(0.f,0.f,1.f), glm::vec2(1.f,0.f),  glm::vec3(0.f,0.f,1.f),
+			glm::vec3(0.0f,0.5f,0.f), glm::vec3(1.f,0.f,0.f), glm::vec2(0.f, 1.f), glm::vec3(1.f,0.f,0.f),
+			glm::vec3(-0.5f,-0.5f,0.f), glm::vec3(0.f,1.f,0.f), glm::vec2(0.f,0.f),glm::vec3(0.f,1.f,0.f),
+			glm::vec3(0.5f,-0.5f,0.f), glm::vec3(0.f,0.f,1.f), glm::vec2(1.f,0.f), glm::vec3(0.f,0.f,1.f)
 		};
 
 		unsigned nrOfVertices = sizeof(vertices) / sizeof(Vertex);
 
-		GLuint indices[] = { 0, 1, 2 };
+		GLuint indices[] = { 0, 1, 2};
 
 
 		unsigned nrOfIndices = sizeof(indices) / sizeof(GLuint);
